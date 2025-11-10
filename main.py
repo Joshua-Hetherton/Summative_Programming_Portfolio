7e4e9483cf91f4b9b80cba546346a975a45fee42
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2025-11-10 09:25:18 +0000

    Adding .gitignore + adding main GUI implementation

--- main.py
+++ main.py
@@ -18,32 +18,44 @@ class GUI:
     def __init__(self, root):
         self.root =  root
         self.setup_window()
-
-    def show_frame(frame):
-        frame.tkraise()
-
         
     def setup_window(self):
         self.root.title("Summative Assignment")
         self.root.geometry("1080x720")
 
+    def show_frame(self, frame):
+        frame.tkraise()
+
+    def all_guis(self):
+        self.rsa_encryption_gui()
+        self.nth_fib_gui()
+        self.sorting_gui()
+        self.brute_force_gui()
+        self.random_deck_gui()
+        self.factorial_calculator_gui()
+        self.search_gui()
+        self.palindrome_gui()
 
 
     #Creates a button that can be clicked to access anything (self, text, colour, position x, position y, command)
-    def create_button(self, text, colour, x_pos, y_pos, command):
-
-        button = tk.Button(self.root, text=text,bg_colour=colour, command=command )
+    def create_button(self, text, x_pos, y_pos, command, colour, parent):
+        button = tk.Button(parent, text=text,bg=colour, command=command )
         #Change to grid format later
         button.place(x=x_pos,y=y_pos)
 
+    def create_label(self, text, x_pos, y_pos, parent, font_size=12):
+        label=tk.Label(parent, text=text, font=("Arial",font_size))
+        label.place(x=x_pos,y=y_pos)
 
-    def create_label(self):
-        pass
+    def create_entry(self,x_pos, y_pos, parent):
+        entry=tk.Entry(parent, x_pos, y_pos, width=30)
+        entry.place(x=x_pos,y=y_pos)
+        return entry
 
-    def create_entry(self):
-        pass
-    
 
+    #Sidebar Implementation
+    """Allows the User to easily navigate between the different algorithms implemented"""
+    def sidebar(self, parent)
 
     #=== Implementation of all required Algorithms ===#
 
@@ -76,10 +88,18 @@ class GUI:
         pass
 
     ##Dynamic Programming
-    def palindrome(self):
+    def palindrome_gui(self):
         pass
     
     #== Design Patterns ==#
     
 
+def main():
+    root=tk.Tk()
+    app=GUI(root)
+    app.all_guis()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
 
