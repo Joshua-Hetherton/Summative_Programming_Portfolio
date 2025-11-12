7d3596568301d03d78c414a92e609367b9acd2b0
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2025-11-12 09:36:54 +0000

    Explanation of Fisher-Yates Shuffle

--- random_deck.py
+++ random_deck.py
@@ -11,5 +11,11 @@ def create_deck():
     full_deck=[f"{number} of {suit}" for suit in suits for number in numbers]
     return full_deck
 
-def Fished_Yates_Shuffle():
+"""An Algorithm that picks a random number 'k' between 1 and Unstruck number remaining in the deck
+Starting from the 0, traverses sequentially , until it reaches an untruck card (and skipps over any struck cards)
+Appends to shuffle output
+Repeat untill all cards are struck
+
+"""
+def Fisher_Yates_Shuffle():
     pass
