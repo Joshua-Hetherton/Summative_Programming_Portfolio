7208363c1ab6f7f20fdfd6fcaf72727d57226439
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-10 11:44:20 +0000

    Refactor: Removed Try-Catch, as in no scenario would it ever be called on

--- brute_force_merge.py
+++ brute_force_merge.py
@@ -8,18 +8,17 @@ def combine_arrays(left_array, right_array):
     output=[]
     left_index=0
     right_index=0
-    try:
-        while (left_index< len(left_array) and right_index < len(right_array)): 
-            if left_array[left_index] <= right_array[right_index]:
-                output.append(left_array[left_index])
-                left_index +=1
-            elif right_array[right_index] <= left_array[left_index]:
-                output.append(right_array[right_index])
-                right_index +=1
-        #Checks for any remaining elements not sorted, by appending any of the remaining elements onto the output
-        output.extend(left_array[left_index:]+ right_array[right_index:])
-    except IndexError:
-        print("Index Error occured when doing merging of the merge sort")
+
+    while (left_index< len(left_array) and right_index < len(right_array)): 
+        if left_array[left_index] <= right_array[right_index]:
+            output.append(left_array[left_index])
+            left_index +=1
+        elif right_array[right_index] <= left_array[left_index]:
+            output.append(right_array[right_index])
+            right_index +=1
+    #Checks for any remaining elements not sorted, by appending any of the remaining elements onto the output
+    output.extend(left_array[left_index:]+ right_array[right_index:])
+
     return output
             
 
