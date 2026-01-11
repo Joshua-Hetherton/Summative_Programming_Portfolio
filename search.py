26072e40e25930a1819f12b4e9011dd5a865126a
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-11 17:08:44 +0000

    chore: Added intial function stubs for search.py

--- search.py
+++ search.py
@@ -2,3 +2,45 @@
 returns the largest, smallest, mode, median, 1st IQF & 3rd IQF of an array.
 User should be able to enter an array of their choice.
 """
+
+def find_largest():
+    """
+    Docstring for find_largest
+    """
+    pass
+
+def find_smallest():
+    """
+    Docstring for find_smallest
+    """
+    pass
+
+def find_median():
+    """
+    Docstring for find_median
+    """
+    pass
+
+def find_mode():
+    """
+    Docstring for find_mode
+    """
+    pass
+
+def find_interquartial_range():
+    """
+    Docstring for find_interquartial_range
+    """
+    pass
+
+def is_array_valid():
+    """
+    Docstring for is_array_valid
+    """
+    pass
+
+def calculate_statistics():
+    """
+    Docstring for calculate_statistics
+    """
+    pass
