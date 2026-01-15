b518fec3868b5a3bb4eeb9fa89375bbaf144e3dd
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-15 09:17:57 +0000

    Feat: Selection sorting algorithm landed. Compares largest/smallest value(via users choice) against the first element of the array thats unsorted

--- sorting.py
+++ sorting.py
@@ -3,6 +3,29 @@ User should be able to enter an array of their choice.
 User should be able to specify Ascending/Descending, default sort if not specified should be Ascending.
 """
 
-def bubble_sort(array,Ascending=True):
-    array.sort()
-    return array
+def bubble_sort(given_array,Ascending=True):
+    given_array.sort()
+    return given_array
+
+def selection_sort(given_array,Ascending=True):
+
+    array_length= len(given_array)
+
+    for i in range(array_length):
+        #current_index is is element currently being compared
+        current_index=i
+        if Ascending:
+            for j in range( i+1, array_length):
+                if given_array[j] < given_array[current_index]:
+                    current_index=j
+        
+        else:
+            for j in range(i+1, array_length):
+                if given_array[j] > given_array[current_index]:
+                    current_index=j
+        #Swapping the found max/min element with the first element
+        given_array[i], given_array[current_index] = given_array[current_index], given_array[i]
+
+    return given_array
+
+        
