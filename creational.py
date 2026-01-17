ebe77eadcf4d02eb24e52054d2051de2203d61d8
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-17 14:08:07 +0000

    Feat: Initial Prototype design pattern

---
+++ creational.py
@@ -0,0 +1,28 @@
+"""
+Creational Design Pattern Module:
+Prototyping Rocket Engines
+"""
+class RocketEngine:
+    def __init__(self, name, engine_type,thrust):
+        self.name=name
+        self.engine_type=engine_type
+        self.thrust=thrust
+        self.burn_time=0
+        self.specific_impulse=0.0
+
+        self.fuel_type=""
+        self.oxidizer_type=""
+        self.mixture_ratio=0.0
+
+        self.features=[]
+
+        pass
+    
+    def clone(self):
+        
+        pass
+    def add_features(self):
+        pass
+
+    def list_features(self):
+        pass
