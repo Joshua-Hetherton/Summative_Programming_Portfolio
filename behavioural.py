0d6d487f13cf9f93917842669c8820a49acd2fd5
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-17 19:14:52 +0000

    Feat: Basic implementation for behavioural pattern

--- behavioural.py
+++ behavioural.py
@@ -1,5 +1,10 @@
 class MissionState:
-
+    def next_state(self,spacecraft):
+        raise NotImplementedError("Subclasses must be implemented")
+    def name(self):
+        raise NotImplementedError("Subclasses must be implemented")
+    def description(self):
+        raise NotImplementedError("Subclasses must be implemented")
     pass
 
 class PrelaunchState(MissionState):
