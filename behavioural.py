b89e16e5ad4025546c4322e663ac012388243fdd
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-17 18:12:29 +0000

    feat: Basic Implementaiton fo behavioural program. Will be simulating spacecraft changing through multiple states(Launch->Reentry)

---
+++ behavioural.py
@@ -0,0 +1,18 @@
+class MissionState:
+
+    pass
+
+class PrelaunchState(MissionState):
+    pass
+
+class LaunchState(MissionState):
+    pass
+
+class OrbitState(MissionState):
+    pass
+
+class ReEntryState(MissionState):
+    pass
+
+class Spacecraft:
+    pass
