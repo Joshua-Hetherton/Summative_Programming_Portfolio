00f6b72e26b53e3e83533ef7c041cbb4a9d64eea
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-01-17 16:10:02 +0000

    Feat: Added Adapter Design Pattern

---
+++ structural.py
@@ -0,0 +1,69 @@
+"""Structural Design Pattern: Adapter Pattern Example"""
+class Measurement:
+    """
+    The Target Interface
+    This is where the client interacts with the desired interface
+    """
+    
+    def get_meters_measurement(self):
+        raise NotImplementedError("Subclass Must be used")
+
+class OriginalMeasurement:
+    """
+    This is the Class which will be adapted to the target interface
+    """
+    def __init__ (self, measurement,unit):
+        self.measurement=measurement
+        self.unit=unit
+
+    def get_measurement(self):
+        return self.measurement
+
+    def get_unit(self):
+        return self.unit
+    
+    pass
+
+class MeasurementAdapter(Measurement):
+    """
+    The adapter class
+    This class adapts the OriginalMeasurement Class to the Target Interface class "Measurement"
+    """
+    def __init__ (self, original_measurement):
+        self.original_measurement=original_measurement
+
+
+    def get_meters_measurement(self):
+        """
+        Where the adaptation happens
+        
+        Args:
+            original_measurement (OriginalMeasurement): The original measurement object to be adapted
+        
+        Returns:
+            float: The measurement in meters
+        """
+        value=self.original_measurement.get_measurement()
+        unit=self.original_measurement.get_unit()
+        if unit=="meters":
+            return value
+        
+        elif unit=="kilometers":
+            return value * 1000
+        
+        elif unit=="gigameters":
+            return value * 1e9
+        
+        elif unit=="miles":
+            return value * 0.000621371
+        
+        elif unit=="feet":
+            return value * 3.28084
+        
+        elif unit=="inches":
+            return value * 39.3701
+    
+        else:
+            return None
+
+    
